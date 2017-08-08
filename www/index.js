49660799dce376116a3ad787c9119a346b37a6c2
Greg Wedow <greg@wedow.cc>
2017-08-08 19:01:24 -0400

    move js to own file and add comment form

---
+++ www/index.js
@@ -0,0 +1,72 @@
+document.body.onload = function() {
+	fetch('/text').then(resp => resp.text()).then(resp => {
+		let posts = JSON.parse(resp)
+		if (posts)
+			posts.forEach(post => renderPost(post))
+	})
+}
+
+document.querySelector('.editor').onsubmit = submitEditor
+
+function submitEditor(e) {
+	e.preventDefault()
+
+	let body = e.currentTarget.querySelector('form [name=text-input]')
+	let user = e.currentTarget.querySelector('form [name=username]')
+
+	let data = {
+		username: user.value,
+		content: body.value
+	}
+
+	fetch('/text',{
+		headers: new Headers({'Content-Type': 'application/json'}),
+		method: 'post',
+		body: JSON.stringify(data)
+	}).then(resp => resp.text()).then(resp => {
+		let post = JSON.parse(resp)
+		renderPost(post)
+		body.value = ''
+	})
+}
+
+function renderPost(post) {
+	let div = document.createElement('div')
+	let metadata = document.createElement('div')
+	let content = document.createElement('div')
+	let hr = document.createElement('hr')
+
+	metadata.append(' - ', post.username, createReplyLink(post.id))
+	content.append(post.content)
+	content.className = "content"
+
+	div.append(content, metadata, hr)
+
+	document.querySelector('#posts').prepend(div)
+}
+
+function createReplyLink(id) {
+	let link = document.createElement('a')
+	link.append('Reply')
+	link.onclick = function(e) {
+		e.preventDefault()
+		let original = document.querySelector('.editor')
+		let editor = original.cloneNode(true)
+
+		let swapBackElements = () => editor.parentNode.replaceChild(link, editor)
+
+		let cancelButton = document.createElement('button')
+		cancelButton.append('Cancel')
+		cancelButton.onclick = swapBackElements
+
+		editor.append(cancelButton)
+		editor.onsubmit = function(e) {
+			submitEditor(e)
+			swapBackElements()
+		}
+
+		link.parentNode.replaceChild(editor, link)
+	}
+
+	return link
+}
