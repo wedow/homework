01454c6828aec10199eb0c5ace2d70dbb66bf499
Greg Wedow <greg@wedow.cc>
2017-08-08 21:28:07 -0400

    add https listener

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"crypto/tls"
+	"golang.org/x/crypto/acme/autocert"
 	"log"
 	"net/http"
 
 	_ "github.com/wedow/homework/db"
 )
 
-var port string = ":8080"
+var domain string = "test.wedow.cc"
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Lshortfile)
@@ -18,6 +20,22 @@ func main() {
 	http.HandleFunc("/echo", echoHandler)
 	http.HandleFunc("/text", textHandler)
 
-	log.Println("Listening on", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Println("Listening on :8080")
+	go http.ListenAndServe(":8080", nil)
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(domain), //your domain here
+		Cache:      autocert.DirCache("certs"),     //folder for storing certificates
+	}
+
+	server := &http.Server{
+		Addr: ":8443",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+
+	log.Println("Listening on :8443", domain)
+	log.Fatal(server.ListenAndServeTLS("", "")) //key and cert are comming from Let's Encrypt
 }
